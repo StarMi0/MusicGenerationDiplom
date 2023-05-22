427caa32966780df7ba9c1aba3f50a28603a792e
StarMi0 <starscream2705@gmail.com>
2023-05-22 17:48:44 +0300

    настройка работы приложения и импорт предикта

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import time
 from flask import Flask, request, jsonify, render_template, make_response
 from flask_restx import Api, Resource, reqparse, api
 from func import gan_melody
+from func.generator import load_trained_model, gan_melody, save_pred
+
+
 
 # создаем приложение
 app = Flask(__name__)
